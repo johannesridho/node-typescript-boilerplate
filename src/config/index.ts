c505e15fc20858c888bc4c21ed43c2d74b7c6ed4
johannesridho <johanridho@gmail.com>
2018-09-27 15:51:18 +0800

    Update port config

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,23 +5,9 @@ dotenv.config({ path: ".env" });
 const config = {
   app: {
     env: process.env.NODE_ENV || "development",
-    port: normalizePort(process.env.PORT || "8080"),
+    port: process.env.PORT || 8080,
     version: process.env.npm_package_version
   }
 };
-
-function normalizePort(val: string) {
-  const parsedPort = parseInt(val, 10);
-
-  if (isNaN(parsedPort)) {
-    return val;
-  }
-
-  if (parsedPort >= 0) {
-    return parsedPort;
-  }
-
-  return false;
-}
 
 export default config;
